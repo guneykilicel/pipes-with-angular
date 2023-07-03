425a701f9f180ab3fb24c6e1efb402494dd6fd52
guneykilicel <kilicelguney@gmail.com>
2023-07-03 16:32:12 +0300

    ders 43 e kadar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Pipes';
+  name='';
+  date='';
+  money:number = 0;
+  height:number = 0;
+  miles:number = 0;
+
+  lessons = {
+    name: 'Math',
+    subject: 'NaturalNumber',
+    currentPoint: 65
+  }
+
+  onNameChange(value:string) {
+    this.name = value;
+  }
+
+  onDateChange(value:string) {
+    this.date = value;
+  }
+
+  onMoneyChange(value:string) {
+    this.money = parseFloat(value);
+  }
+
+  onHeightChange(value:string) {
+    this.height = parseFloat(value);
+  }
+
+  onMilesChange(value:string) {
+    this.miles = parseFloat(value);
+  }
 }
